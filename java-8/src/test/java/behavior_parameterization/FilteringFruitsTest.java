fb0bcb08aea51067720553645fca8e3528746170
Namjun Kim <namjunemy@gmail.com>
2019-10-20 13:32:54 +0900

    junit 5 적용, 실행어라운드 패턴 설명 보강

--- java-8/src/test/java/behavior_parameterization/FilteringFruitsTest.java
+++ java-8/src/test/java/behavior_parameterization/FilteringFruitsTest.java
@@ -1,6 +1,6 @@
 package behavior_parameterization;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
--- java-8/src/test/java/lambda_ex/ReaderTest.java
+++ java-8/src/test/java/lambda_ex/ReaderTest.java
@@ -1,7 +1,7 @@
 package lambda_ex;
 
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,6 +9,7 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReaderTest {
+
     /**
      * 추상 메서드가 오직 하나면 함수형 인터페이스이다.(디폴트 메서드 가능)
      */
@@ -36,6 +37,14 @@ public class ReaderTest {
     }
 
     /**
+     * 실행 어라운드 패턴
+     * 리소스 설정과 정리 과정은 재사용하고 processFile 메서드만 동작 파라미터화 해서 다른 동작을 수행할 수 있다.
+     *
+     * 동작에 해당하는 BufferedReader -> String, throws IOException 메서드 시그니처를 던질 수 있는
+     * 함수형 인터페이스(BufferedReaderProcessor)를 만들어야 한다. 그러면, processFile 의 인수로 인터페이스(익명클래스, 람다)를 전달할 수 있다.
+     * 결과적으로 함수형 인터페이스 BufferedReaderProcessor 에 정의된
+     * process 메서드의 시그니처와 일치하는 람다(BufferedReader -> String)를 전달 할 수 있다.
+     *
      * BufferedReaderProcessor 에 정의된 processor 메서드의 시그니처 (BufferedReader) -> String 과
      * 일치하는 람다를 전달해서 함수형 인터페이스의 인스턴스로 전달된 코드와 같은 방식으로 처리한다.
      *

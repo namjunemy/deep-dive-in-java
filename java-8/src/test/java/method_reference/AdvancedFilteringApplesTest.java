1f134c9e7d1af0e83d870a5095d139e1cad86e72
Namjun Kim <namjunemy@gmail.com>
2019-10-24 01:47:45 +0900

    List sorting 단계별 간소화(동작 파라미터화, 익명 클래스, 람다, 메서드 레퍼런스)

---
+++ java-8/src/test/java/method_reference/AdvancedFilteringApplesTest.java
@@ -0,0 +1,79 @@
+package method_reference;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AdvancedFilteringApplesTest {
+
+    private List<String> list;
+
+    @BeforeEach
+    void setUp() {
+        list = Arrays.asList("c", "b", "a", "d");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("1단계")
+    void 동작_파라미터화() {
+        list.sort(new CustomComparator());
+        assertThat(list.get(0)).isEqualTo("a");
+        assertThat(list.get(3)).isEqualTo("d");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("2단계")
+    void 익명_클래스_사용() {
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareToIgnoreCase(o1);
+            }
+        });
+
+        assertThat(list.get(0)).isEqualTo("d");
+        assertThat(list.get(3)).isEqualTo("a");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("3단계")
+    void 람다_표현식_사용() {
+        list.sort((String o1, String o2) -> o1.compareToIgnoreCase(o2));
+        assertThat(list.get(0)).isEqualTo("a");
+        assertThat(list.get(3)).isEqualTo("d");
+
+        list.sort((o1, o2) -> o2.compareToIgnoreCase(o1));
+        assertThat(list.get(0)).isEqualTo("d");
+        assertThat(list.get(3)).isEqualTo("a");
+
+        //가독성 향상(Comparator의 comparing 정적메서드 사용)
+        Comparator<String> c = Comparator.comparing((String s) -> s);
+        list.sort(c);
+        assertThat(list.get(0)).isEqualTo("a");
+        assertThat(list.get(3)).isEqualTo("d");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("4단계")
+    void 메서드_레퍼런스_사용() {
+        // Comparator의 comparing 메서드를 메서드 레퍼런스로 변경
+        list.sort(Comparator.comparing(Function.identity()));
+        assertThat(list.get(0)).isEqualTo("a");
+        assertThat(list.get(3)).isEqualTo("d");
+    }
+}
---
+++ java-8/src/test/java/method_reference/CustomComparator.java
@@ -0,0 +1,50 @@
+package method_reference;
+
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+public class CustomComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.compareToIgnoreCase(o2);
+    }
+
+    @Override
+    public Comparator<String> reversed() {
+        return null;
+    }
+
+    @Override
+    public Comparator<String> thenComparing(Comparator<? super String> other) {
+        return null;
+    }
+
+    @Override
+    public <U> Comparator<String> thenComparing(Function<? super String, ? extends U> keyExtractor, Comparator<? super U> keyComparator) {
+        return null;
+    }
+
+    @Override
+    public <U extends Comparable<? super U>> Comparator<String> thenComparing(Function<? super String, ? extends U> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<String> thenComparingInt(ToIntFunction<? super String> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<String> thenComparingLong(ToLongFunction<? super String> keyExtractor) {
+        return null;
+    }
+
+    @Override
+    public Comparator<String> thenComparingDouble(ToDoubleFunction<? super String> keyExtractor) {
+        return null;
+    }
+}
